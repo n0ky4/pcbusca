773d587d2587a74a2d530ee01ac20cef76f17b2c
nokya <n0ky4@proton.me>
2025-01-09 15:44:50 -0300

    [feat] modal transitions & backdrop

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { X } from 'lucide-react'
 import type { PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -14,17 +14,24 @@ interface ModalProps extends PropsWithChildren {
 export function Modal({ show, onClose, children, title, description }: ModalProps) {
     return (
         <Dialog open={show} onClose={onClose} className='relative z-50'>
+            <DialogBackdrop
+                transition
+                className='fixed inset-0 bg-slate-950/75 duration-200 ease-out data-[closed]:opacity-0 backdrop-blur-lg'
+            />
             <div
                 className={twMerge(
-                    'fixed top-0 left-0 flex bg-black/25',
+                    'fixed top-0 left-0 flex',
                     'w-screen h-screen justify-center p-4 lg:pt-32 pt-16'
                 )}
             >
                 <DialogPanel
+                    transition
                     className={twMerge(
+                        'duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0',
                         'max-w-screen-sm h-fit w-full p-8 rounded-lg',
                         'bg-slate-900 border border-slate-800',
-                        'flex flex-col gap-4'
+                        'flex flex-col gap-4',
+                        'shadow-lg'
                     )}
                 >
                     <div>
